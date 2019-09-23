4eb812ebe86444b78916c85ee3d9e27c45e6c6a4
Sk-Rahul <55654071+Sk-Rahul@users.noreply.github.com>
2019-09-23 15:14:32 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,25 @@
-alert("hello World ");
+const softkeyCallback = {
+    left: function() { alert('You click on SoftLeft') },
+    center: function() { alert('You click on Enter') },
+    right: function() { alert('You click on SoftRight') }
+};
+function handleKeyDown(evt) {
+    switch (evt.key) {
+        case 'SoftLeft':
+            // Action case press left key
+            softkeyCallback.left();
+        break;
+
+        case 'SoftRight':
+            // Action case press right key
+            softkeyCallback.right();
+        break;
+
+        case 'Enter':
+            // Action case press center key
+            softkeyCallback.center();
+        break;
+    }
+};
+
+document.addEventListener('keydown', handleKeyDown);
