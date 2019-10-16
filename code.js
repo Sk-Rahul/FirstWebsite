7250a686b9f3e47e36a207669a4efd2cbcc2b8cd
Sk-Rahul <55654071+Sk-Rahul@users.noreply.github.com>
2019-10-16 17:07:27 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,24 @@
-
+document.addEventListener('keydown', handleKeyup);
+	
+	function handleKeyup (e) {
+		switch(e.key) {
+			case 'ArrowUp':
+			case '2': /* num pad navigation */
+				app.keyCallback.dUp();
+				alert("ArrowUp");
+				break;
+			case 'ArrowDown':
+			case '8': /* num pad navigation */
+				app.keyCallback.dDown();
+				alert("ArrowDown");
+				break;
+			case 'ArrowLeft': 
+			case '4': /* num pad navigation */
+				app.keyCallback.dLeft();
+				alert("arrow left");
+				break;
+			case 'ArrowRight':
+			case '6': /* num pad navigation */
+				alert("ArrowRight");
+				app.keyCallback.dRight();
+				break;
