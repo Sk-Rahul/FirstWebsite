1a75c8a0d982c8e6fd1c83451b4b5bb2cf1cf071
Sk-Rahul <55654071+Sk-Rahul@users.noreply.github.com>
2019-11-02 10:52:14 +0530

    Add files via upload

---
+++ Addimage.js
@@ -0,0 +1,15 @@
+
+  
+  document.getElementById("myImg").src = "icon56.png";
+  document.body.style.backgroundColor = "black";
+  var x = "PLEASE CHECK YOUR INTERNET CONNECTION";
+  document.getElementById("text").innerHTML = x;
+ 
+
+
+var s =
+{
+   t : "First",
+};
+
+ShowImage();
